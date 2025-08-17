555d6f550ded3eb51a64d4055e0166c5e567c358
Dalton <daltonfiggdavies@gmail.com>
2025-08-17 13:59:59 +0100

    Generate map and add all wildfire markers

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,19 +1,35 @@
 document.addEventListener("DOMContentLoaded", async () => {
-  // fetch all open EONET wildfire events in the past 14 days
   const wildfireData = await fetchData(
-    "https://eonet.gsfc.nasa.gov/api/v3/events?category=wildfires&status=open&days=14"
+    "https://eonet.gsfc.nasa.gov/api/v3/events?category=wildfires&status=open&days=31"
   );
 
   const wildfires = wildfireData.events;
 
-  console.log(wildfires);
+  const map = L.map("map").setView([0, 0], 2);
+
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+
+  for (const wildfire of wildfires) {
+    const geometry = wildfire.geometry?.[0];
+    if (!geometry || !geometry.coordinates) return;
+
+    const [lng, lat] = wildfire.geometry[0].coordinates;
+    const title = wildfire.title;
+
+    const marker = L.marker([lat, lng], { alt: title }).addTo(map);
+    marker.bindPopup(title);
+  }
 });
 
 const fetchData = async (url) => {
   try {
     const response = await fetch(url);
 
-    if (!response.ok) throw new Error(response);
+    if (!response.ok) throw new Error(response.statusText);
 
     const data = await response.json();
 
